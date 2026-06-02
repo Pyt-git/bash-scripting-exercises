d2d64c04cfac547268bc55073ca107e5f890437a
Pyt-git <weicky0804@gmail.com>
2026-06-02 17:45:31 -0400

    Update file_check.sh

--- bash_scripting/file_check.sh
+++ bash_scripting/file_check.sh
@@ -7,12 +7,6 @@ else
     echo "1 is not a file"
 fi
 
-# code that checks whether paramater is a directory
-if [-d "$1"]; then
-    echo "1 is a directory"
-else
-    echo "1 is not a directory"
-fi
 
 # code that checks whether the parameter is other than a file or a directory
 if [[! (-f "$1" || -d "$1")]]; then
