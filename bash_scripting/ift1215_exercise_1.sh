160fd693b2ae06ca725b1d6df5f3b44e3546eaf8
Pyt-git <weicky0804@gmail.com>
2026-06-02 14:07:04 -0400

    Update ift1215_exercise_1.sh

--- bash_scripting/ift1215_exercise_1.sh
+++ bash_scripting/ift1215_exercise_1.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+# code that checks whether parameter is a file
+if [-f "$1"]; then
+    echo "1 is a file"
+else
+    echo "1 is not a file"
+fi
+
+# code that checks whether paramater is a directory
+if [-d "$1"]; then
+    echo "1 is a directory"
+else
+    echo "1 is not a directory"
+fi
+
+# code that checks whether the parameter is other than a file or a directory
+if [[! (-f "$1" || -d "$1")]]; then
+    echo "1 is neither a file nor a directory"
+else
+    echo "x is e"
+fi
