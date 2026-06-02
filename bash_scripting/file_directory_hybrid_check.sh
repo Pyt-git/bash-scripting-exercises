b545b4318f72debd3a27e621ecc6524e15b9c79c
Pyt-git <weicky0804@gmail.com>
2026-06-02 17:46:04 -0400

    Update file_directory_hybrid_check.sh

--- bash_scripting/file_directory_hybrid_check.sh
+++ bash_scripting/file_directory_hybrid_check.sh
@@ -1,1 +1,9 @@
+# !/bin/bash
+
+# code that checks whether the parameter is other than a file or a directory
+if [[! (-f "$1" || -d "$1")]]; then
+    echo "1 is neither a file nor a directory"
+else
+    echo "x is e"
+fi
 
